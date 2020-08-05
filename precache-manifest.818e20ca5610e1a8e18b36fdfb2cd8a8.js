2b027b85ffd7ca3e3de8e853e00ecd7f95c7a716
mariya <mariyafareed1705@gmail.com>
2020-08-05 21:16:31 +0530

    Updates

---
+++ precache-manifest.818e20ca5610e1a8e18b36fdfb2cd8a8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746dab6e67a14499efdff131eb0e6321",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfd7ae50a4daff99982f",
+    "url": "/static/js/2.1af805d2.chunk.js"
+  },
+  {
+    "revision": "3e771ed55fe82c471e0a",
+    "url": "/static/js/main.ed1ec7ea.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
